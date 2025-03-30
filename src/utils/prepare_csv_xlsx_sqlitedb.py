71b07fb697e78a033639bca3817b29784e9ec80a
tharindu-kurukulasooriya-randoli <tharindu.kurukulasooriya@randoli.ca>
2025-03-30 22:56:00 +0530

    second commit

--- src/utils/prepare_csv_xlsx_sqlitedb.py
+++ src/utils/prepare_csv_xlsx_sqlitedb.py
@@ -1,107 +1,70 @@
-import os 
-import pandas as pd 
-import yaml 
-from pyprojroot import here   # We use this to find the root directory ot our project 
-# here() returns a function that constructs absolute paths relative to the project root directory 
-from dotenv import load_dotenv
-import chromadb
-from dotenv import load_dotenv
-from openai import OpenAI
+import os
+import pandas as pd
+from utils.load_config import LoadConfig
+import pandas as pd
+from sqlalchemy import create_engine, inspect
 
 
-load_dotenv(dotenv_path=here(".env"))
+class PrepareSQLFromTabularData:
+    """
+    A class that prepares a SQL database from CSV or XLSX files within a specified directory.
 
-
-
-
-class LoadConfig:
-    def __init__(self) -> None:
-        with open(here("configs/app_config.yml")) as cfg:
-            app_config = yaml.load(cfg, Loader=yaml.FullLoader)
-
-        self.load_directories(app_config=app_config)
-        self.load_llm_configs(app_config=app_config)
-        self.load_openai_models()
-        self.load_chroma_client()
-        self.load_rag_config(app_config=app_config)
-
-        # If we  want to clean up the upload csv SQL DB on every fresh run of the chatbot we can uncomment the next line . (if it exists)
-        # self.remove_directory(self.uploaded_files_sqldb_directory)
-
-
-
-    def load_directories(self, app_config):
-        self.stored_csv_xlsx_directory = here(
-            app_config["directories"]["stored_csv_xlsx_directory"])
-        self.sqldb_directory = str(here(
-            app_config["directories"]["sqldb_directory"]))
-        self.uploaded_files_sqldb_directory = str(here(
-            app_config["directories"]["uploaded_files_sqldb_directory"]))
-        self.stored_csv_xlsx_sqldb_directory = str(here(
-            app_config["directories"]["stored_csv_xlsx_sqldb_directory"]))
-        self.persist_directory = app_config["directories"]["persist_directory"]
-
-
-
-
-    def load_llm_configs(self, app_config):
-        self.model_name = os.getenv("gpt_deployment_name")
-        self.agent_llm_system_role = app_config["llm_config"]["agent_llm_system_role"]
-        self.rag_llm_system_role = app_config["llm_config"]["rag_llm_system_role"]
-        self.temperature = app_config["llm_config"]["temperature"]
-        self.embedding_model_name = os.getenv("embed_deployment_name")
-
-
-
-
-    def load_openai_models(self):
-        openai_api_key = os.environ["OPENAI_API_KEY"]
-
-        # Initialize OpenAI client
-        self.openai_client = OpenAI(api_key=openai_api_key)
-
-        #  callable for generating completions
-        self.chat = lambda messages: self.openai_client.chat.completions.create(
-            model=self.model_name,
-            messages=messages,
-            temperature=self.temperature
-        )
-
-        #  callable for embeddings
-        self.embed = lambda input_text: self.openai_client.embeddings.create(
-            model=self.embedding_model_name,
-            input=input_text
-        ).data[0].embedding
-
-
-
-    def load_chroma_client(self):
-        self.chroma_client = chromadb.PersistentClient(
-            path=str(here(self.persist_directory)))
-
-    def load_rag_config(self, app_config):
-        self.collection_name = app_config["rag_config"]["collection_name"]
-        self.top_k = app_config["rag_config"]["top_k"]
-
-    def remove_directory(self, directory_path: str):
+    This class reads each file, converts the data to a DataFrame, and then
+    stores it as a table in a SQLite database, which is specified by the application configuration.
+    """
+    def __init__(self, files_dir) -> None:
         """
-        Removes the specified directory.
+        Initialize an instance of PrepareSQLFromTabularData.
 
-        Parameters:
-            directory_path (str): The path of the directory to be removed.
-
-        Raises:
-            OSError: If an error occurs during the directory removal process.
-
-        Returns:
-            None
+        Args:
+            files_dir (str): The directory containing the CSV or XLSX files to be converted to SQL tables.
         """
-        if os.path.exists(directory_path):
-            try:
-                shutil.rmtree(directory_path)
-                print(
-                    f"The directory '{directory_path}' has been successfully removed.")
-            except OSError as e:
-                print(f"Error: {e}")
-        else:
-            print(f"The directory '{directory_path}' does not exist.")
+        APPCFG = LoadConfig()
+        self.files_directory = files_dir
+        self.file_dir_list = os.listdir(files_dir)
+        db_path = APPCFG.stored_csv_xlsx_sqldb_directory
+        db_path = f"sqlite:///{db_path}"
+        self.engine = create_engine(db_path)
+        print("Number of csv files:", len(self.file_dir_list))
+
+    def _prepare_db(self):
+        """
+        Private method to convert CSV/XLSX files from the specified directory into SQL tables.
+
+        Each file's name (excluding the extension) is used as the table name.
+        The data is saved into the SQLite database referenced by the engine attribute.
+        """
+        for file in self.file_dir_list:
+            full_file_path = os.path.join(self.files_directory, file)
+            file_name, file_extension = os.path.splitext(file)
+            if file_extension == ".csv":
+                df = pd.read_csv(full_file_path)
+            elif file_extension == ".xlsx":
+                df = pd.read_excel(full_file_path)
+            else:
+                raise ValueError("The selected file type is not supported")
+            df.to_sql(file_name, self.engine, index=False)
+        print("==============================")
+        print("All csv files are saved into the sql database.")
+
+    def _validate_db(self):
+        """
+        Private method to validate the tables stored in the SQL database.
+
+        It prints out all available table names in the created SQLite database
+        to confirm that the tables have been successfully created.
+        """
+        insp = inspect(self.engine)
+        table_names = insp.get_table_names()
+        print("==============================")
+        print("Available table nasmes in created SQL DB:", table_names)
+        print("==============================")
+
+    def run_pipeline(self):
+        """
+        Public method to run the data import pipeline, which includes preparing the database
+        and validating the created tables. It is the main entry point for converting files
+        to SQL tables and confirming their creation.
+        """
+        self._prepare_db()
+        self._validate_db()
